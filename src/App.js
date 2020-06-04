6741252fdf9c49971c3ba5e03a002f55a838e04c
cartel360 <billycartel360@gmail.com>
2020-06-04 13:06:24 +0300

    Added Checkbox and Line-Through

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > Todo List < /h1> < /
-        div >
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Do some Project',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Clean My Laptop',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Submit An assignment',
+        completed: false
+      }
+    ]
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <Todos todos={this.state.todos} />
+      </div>
     );
+  }
 }
+
 
 export default App;
